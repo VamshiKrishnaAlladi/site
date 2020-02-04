d7f8aa597a1b0d4d7088545803ef07a3362334e7
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2020-02-04 10:47:45 +0530

    feat(404-page): implement a 404 PageNotFound component

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./PageNotFound.css";
+
+export const PageNotFound = () => ( <>
+    <p className="seriously">
+        Seriously?!
+        <br/>
+        What were you expecting to find here?
+        <br/><br/>
+        I am intrigued now. 🤔<br/> Was this a wild guess or a calculated one?
+    </p>
+</> );
