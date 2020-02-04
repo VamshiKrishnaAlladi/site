d36621dd15823e4c853e66e388461b3a2571b2d3
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2020-02-04 11:04:19 +0530

    improvement: accept banner width and height from parent component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,8 +4,8 @@ import { Logo } from "./Logo";
 
 import "./Banner.css";
 
-export const Banner = () => (
-    <div className="banner">
-        <Logo className="banner-logo" width="100%" height="100%" />
+export const Banner = ({width = "100%", height = "100%"}) => (
+    <div className="banner" id="banner">
+        <Logo className="banner-logo" width={width} height={height} />
     </div>
 );
