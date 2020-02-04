37f37f3e2b0012369ac8a39bdff28242d93cb5a6
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2020-02-04 10:46:19 +0530

    feat: implement a place-holder component for in-progress views

---
+++ src/components/UnderConstruction.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./UnderConstruction.css"
+
+export const UnderConstruction = () => ( <>
+    <p className="under-construction">
+        This page is still under construction...
+        <br />
+        Come again later...
+    </p>
+</> );
